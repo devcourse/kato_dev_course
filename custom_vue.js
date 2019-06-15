35b1b2225f47bcba04171f91e29d8950f5c9f3a6
Rafał Jaseniuk <biuro@encodeit.pl>
2019-06-15 14:08:08 +0200

    update

--- custom_vue.js
+++ custom_vue.js
@@ -8,6 +8,11 @@ const section_two = new Vue({
 	el: "#section_two",
 	data: {
 		name: 'Nazwa pierwsza',
-		bool: true
+		bool: true,
+		buttonCategory: 'przycisk normalny',
+		buttonCategoryTwo: 'inny przycisk',
+		another: 1,
+		buttonOne: "btn btn-primary"
 	}
 })
+
