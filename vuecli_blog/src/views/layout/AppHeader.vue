3c94b52fa93167a751e709b023eee96c7c19da92
Rafał Jaseniuk <biuro@encodeit.pl>
2019-06-30 16:39:25 +0200

    dynamic menuOne and menuTwo, styling AppHeader.vue

--- vuecli_blog/src/views/layout/AppHeader.vue
+++ vuecli_blog/src/views/layout/AppHeader.vue
@@ -3,37 +3,44 @@
 		<div class="head-nav">
 
 			<div class="container">
-				<div class="row">
+				<div class="row align-items-center">
 					<div class="col-6">
+
 						<div class="head-nav-left">
 							<div class="head-nav-left-single">
 								June 29, 2019
 							</div>
-							<div class="head-nav-left-single">
-								HOME
-							</div>
-							<div class="head-nav-left-single">
-								ABOUT US
-							</div>
-							<div class="head-nav-left-single">
-								TEAM
-							</div>
+
+							<router-link
+								v-for="link in menuTwo" 
+								:key="link.id"
+								class="head-nav-left-single":to="link.route">
+								{{link.name}}
+							</router-link>	
+
+
 						</div>
 
 					</div>
 
 					<div class="col-6">
 						<div class="head-nav-right">
+				<div class="head-nav-right-single">
+					<img
+						class="head-nav-right-single-img"
+						src="@/assets/icon-fb.png" alt="">
+				</div>
+
 							<div class="head-nav-right-single">
-								<img src="@/assets/icon-fb.png" alt="">
+								<img
+									class="head-nav-right-single-img"
+									src="@/assets/icon-instagram.png" alt="">
 							</div>
 
 							<div class="head-nav-right-single">
-								<img src="@/assets/icon-instagram.png" alt="">
-							</div>
-
-							<div class="head-nav-right-single">
-								<img src="@/assets/icon-twitter.png" alt="">
+								<img
+									class="head-nav-right-single-img"
+									src="@/assets/icon-twitter.png" alt="">
 							</div>
 						</div>
 					</div>
@@ -46,15 +53,26 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-2">
-						<img class="head-navigation-logo-img" src="" alt="">
+						<img 
+							class="head-navigation-logo-img" 
+							src="@/assets/logo.png" 
+							alt="">
 					</div>
 					<div class="col-10">
 						<ul class="head-navigation-ul">
 							<li class="head-navigation-ul-li">
-								<router-link to="/"> Home </router-link>
+								<router-link
+									class="head-navigation-ul-li-a"
+									to="/">
+									Home
+								</router-link>
 							</li>
 							<li class="head-navigation-ul-li">
-								<router-link to="/about"> About </router-link>
+								<router-link
+									class="head-navigation-ul-li-a"
+									to="/about"> 
+									About
+								</router-link>
 							</li>
 						</ul>	
 					</div>
@@ -66,12 +84,26 @@
 
 <script>
 export default {
-	name: 'AppHeader'
+	name: 'AppHeader',
+	data () {
+		return {
+			menuTwo: [
+				{
+					name: 'Home',
+					route: '/'
+				},
+				{
+					name: 'About',
+					route: '/about'
+				}
+			]
+		}
+	}
 }
 </script>
 <style>
 .head-nav {
-	height: 40px;
+	padding:5px 0; 
 	background: #222222;
 	color:white;
 	display: flex;
@@ -85,10 +117,19 @@ export default {
 	justify-content: flex-start;
 }
 .head-nav-left-single {
+	font-size: 12px;
 	margin-right: 20px;
+	color: rgb(187,187,187);
+	text-decoration: none;
+	text-transform: uppercase;
+	transition: color .4s;
 }
 .head-nav-left-single:first-child {
 	color: #ff4dac;
+}
+.head-nav-left-single:hover {
+	text-decoration: none;
+	color:#fff;
 }
 .head-nav-right {
 	justify-content: flex-end;
@@ -97,17 +138,35 @@ export default {
 	align-content: flex-end;
 	margin-left: 20px;
 }
+.head-nav-right-single-img {
+	max-width: 60%;
+}
 .head-navigation {
-	height: 100px;
+	padding: 10px 0;
 	box-shadow: 0px 3px 13px -8px rgba(0,0,0,0.75);
+}
+.head-navigation-logo-img {
+	max-width: 100%;
 }
 .head-navigation-ul {
 	list-style-type: none;
 	display: flex;
 	justify-content: flex-end;
+	height: 100%;
+    align-items: center;
+    margin: 0;
 }
 .head-navigation-ul-li {
+	text-transform: uppercase;
 	margin-left:15px;
+}
+.head-navigation-ul-li > .head-navigation-ul-li-a  {
+	color: black;
+	text-decoration: none;
+	transition: color .4s;
+}
+.head-navigation-ul-li:hover > .head-navigation-ul-li-a  {
+	color: #e91e63;
 }
 .head-navigation-ul:last-child {
 	margin-left:0;
