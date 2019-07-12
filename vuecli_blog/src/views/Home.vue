bb6029f5091c3f8a00ff791814b361c60af877b2
Rafał Jaseniuk <biuro@encodeit.pl>
2019-07-12 10:53:53 +0200

    single-post divs

--- vuecli_blog/src/views/Home.vue
+++ vuecli_blog/src/views/Home.vue
@@ -3,23 +3,60 @@
 		<div class="container">
 			<!-- kod emmet -->
 <!-- .row>.single-post>.col-md-6.col-12+.col-md-6.col-12 -->
-			<div class="row">
+			
 				<!-- dokończyć in da hause -->
 				<div
 					v-for="post in posts"
 					:key="post.id" 
 					class="single-post">
-					<div class="col-md-6 col-12">
-						<!-- strona z darmowymi zdjęciami bardzo dobrej jakości -->
-						<!-- https://unsplash.com/ -->
-						<img
-							class="single-post-left-img"
-							src="@/assets/single-post.jpg"
-							alt="">
+
+<div class="row">
+	<div class="col-md-6 col-12">
+		<!-- strona z darmowymi zdjęciami bardzo dobrej jakości -->
+		<!-- https://unsplash.com/ -->
+		<img
+			class="single-post-left-img"
+			src="@/assets/single-post.jpg"
+			alt="">
+	</div>
+	<div class="col-md-6 col-12">
+
+
+		<div class="single-post-right">
+			<h3 class="single-post-right-title">
+				Nazwa wpisu
+			</h3>	
+			<span class="single-post-right-date">
+				12.07.2019
+			</span>
+			<p class="single-post-right-desc">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio ipsam ex
+			</p>
+			<div class="single-post-right-footer">
+				<div class="single-post-right-footer-left">
+					<a
+						class="single-post-right-footer-left-a"
+						href="#">
+							Czytaj więcej
+						</a>
+				</div>
+				<div class="single-post-right-footer-right">
+					<div class="single-post-right-footer-right-comments">
+						<img src="" alt="">
+						<span class="single-post-right-footer-right-comments-count">
+							0
+						</span>
 					</div>
-					<div class="col-md-6 col-12"></div>
 				</div>
 			</div>
+		</div>
+
+	</div>
+</div>
+
+
+
+				</div>
 		</div>
 	</section>
 </template>
@@ -67,6 +104,9 @@ export default {
 }
 </script>
 <style>
+.single-post {
+	margin: 20px 0;
+}
 .single-post-left-img {
 	width: 100%;
 	border-radius: 8px;
