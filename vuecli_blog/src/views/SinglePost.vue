0ae756bf65a423bd31e43b42eed45a6fb940994d
Rafał Jaseniuk <biuro@encodeit.pl>
2019-07-13 12:29:36 +0200

    added some style with paralax img to singlepost.vue

--- vuecli_blog/src/views/SinglePost.vue
+++ vuecli_blog/src/views/SinglePost.vue
@@ -1,19 +1,45 @@
 <template>
 	<section class="single-post">
-		tytuł: <br>
+
+
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="single-post-left">
+
+						<div class="sinlge-post-left-header"></div>
+						<div class="sinlge-post-left-content"></div>
+					
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div
+						:style="{ 'backgroundImage': 'url(' + imageParalax + ')' }"
+						class="single-post-right-img"
+						alt="">
+						</div>
+				</div>
+			</div>
+		</div>
+
+
+<!-- 		tytuł: <br>
 		{{singlePost.title}} <br><br>
 		treść: <br>
-		{{singlePost.body}}
+		{{singlePost.body}} -->
+
 	</section>
 </template>
 <script>
+	import paralaxImg from '@/assets/single-post.jpg'
 	import axios from 'axios'
 	export default {
 		name: 'singlePost',
 		props: ['postId'],
 		data () {
 			return {
-				singlePost: []
+				singlePost: [],
+				imageParalax: paralaxImg
 			}
 		},
 		/*
@@ -29,7 +55,7 @@
 			getSinglePost() {
 				return axios.get('https://jsonplaceholder.typicode.com/posts/' + this.postId)
 			},
-			// kod starej metody pobierania zmiiennych 
+			// kod starej metody pobierania zmiennych 
 			// przekazanych w params
 			// fetchData () {
 			// 	this.postId = this.$route.params.postId
@@ -52,4 +78,13 @@
 		}
 	}
 </script>
-<style></style>
+<style>
+	.single-post-right-img {
+		height: 600px;
+		width: 100%;
+		background-attachment: fixed;
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;		
+	}
+</style>
